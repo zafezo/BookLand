dea7f1050a50e77bd0c37a5a9d83cfdd30b60d3c
zafezo <nazarslbdn@gmail.co>
2015-09-27 20:11:36 +0300

    SingUp_v_0.3

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'signup' => 'users#new'
   get  'help' => 'static_pages#help'
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
